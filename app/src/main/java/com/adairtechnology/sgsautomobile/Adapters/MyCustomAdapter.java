bc66640fdaa6385375f091542376d5405ff188da
SivaShankari <sivashankari.moorthy@i3software.in>
2017-04-28 12:56:49 +0530

    "Using Parameter In Adapter"

--- app/src/main/java/com/adairtechnology/sgsautomobile/Adapters/MyCustomAdapter.java
+++ app/src/main/java/com/adairtechnology/sgsautomobile/Adapters/MyCustomAdapter.java
@@ -19,6 +19,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 
 import static android.content.Context.MODE_PRIVATE;
+import static com.adairtechnology.sgsautomobile.ActivityClasses.Activity_Inward_Entry_Screen.parmeter;
 import static com.adairtechnology.sgsautomobile.ActivityClasses.Activity_Inward_Entry_Screen.tex_rowCount;
 import static com.adairtechnology.sgsautomobile.ActivityClasses.Activity_Inward_Entry_Screen.txt_total;
 
@@ -31,18 +32,16 @@ public class MyCustomAdapter extends ArrayAdapter<ListItem> {
     private ArrayList<ListItem> arraylist;
     private List<ListItem> itemList;//arrayList.addAll(itemlist)
     private Context mContext;
-    public int outQuty;
-    String code,name,quentity,rate,dist;
-    String purName,purcode,purQuty,purrate,purdist;
+
     int rowBeforeDel;
     public ArrayList<Integer> Quantity;
 
 
     public int quantitys;
-    public static int delQutya;
-//    public static int test;
 
     ArrayList<String> testItem = new ArrayList<>();
+    ArrayList<String> testItems = new ArrayList<>();
+
     private ArrayList<ListItem> list;
 
     private String all_details;
@@ -54,21 +53,13 @@ public class MyCustomAdapter extends ArrayAdapter<ListItem> {
         super(context ,0,itemList);
         this.itemList = itemList;
         mContext = context;
-//        mLayoutInflater = LayoutInflater.from(context);
         this.arraylist = new ArrayList<ListItem>();
         this.arraylist.addAll(itemList);
-      //  System.out.println("the Item list value is :" + itemList);
-
-
 
     }
 
-
     @Override
     public ListItem getItem(int position) {
-    /*    quantity = Integer.parseInt(itemList.get(position).getIteQuty());
-        System.out.println("The total quantity of ADA : " + MyCustomAdapter.quantity);*/
-
 
         return itemList.get(position);
     }
@@ -76,13 +67,6 @@ public class MyCustomAdapter extends ArrayAdapter<ListItem> {
 
     @Override
     public long getItemId(int position) {
-
-
-        rowBeforeDel = itemList.size();
-        System.out.println(" list size is" + rowBeforeDel);
-
-
-
         return position;
     }
 
@@ -90,120 +74,225 @@ public class MyCustomAdapter extends ArrayAdapter<ListItem> {
     public int getCount() {
         rowBeforeDel = itemList.size();
         System.out.println(" list size is" + rowBeforeDel);
-
-
-
+        tex_rowCount.setText("TotalCount: " +rowBeforeDel);
      return itemList.size();
     }
 
     @Override
     public View getView(final int position, View view, ViewGroup viewGroup) {
+        final String code, name, quentity, rate, dist;
+        String purName, purcode, purQuty, purrate, purdist;
         int test2 = 0;
         ViewHolder holder;
-        if (view == null) {
-            view = LayoutInflater.from(mContext).inflate(R.layout.activity_main_custom, viewGroup, false);
-            holder = new ViewHolder();
-            Quantity = new ArrayList<Integer>();
-            holder.itemname = (TextView) view.findViewById(R.id.txtnames);
-            holder.itemcode = (TextView) view.findViewById(R.id.txtCodes);
-            holder.itemqut = (TextView) view.findViewById(R.id.txtQuty);
-            holder.itemdis = (TextView) view.findViewById(R.id.txtDisc);
-            holder.itemrate = (TextView) view.findViewById(R.id.txtRate);
-            holder.img_delete = (ImageView)view.findViewById(R.id.delete);
+        if (parmeter.equals(" ")) {
+            if (view == null) {
+                view = LayoutInflater.from(mContext).inflate(R.layout.activity_main_custom, viewGroup, false);
+                holder = new ViewHolder();
+                Quantity = new ArrayList<Integer>();
+                holder.itemname = (TextView) view.findViewById(R.id.txtnames);
+                holder.itemcode = (TextView) view.findViewById(R.id.txtCodes);
+                holder.itemqut = (TextView) view.findViewById(R.id.txtQuty);
+                holder.itemdis = (TextView) view.findViewById(R.id.txtDisc);
+                holder.itemrate = (TextView) view.findViewById(R.id.txtRate);
+                holder.img_delete = (ImageView) view.findViewById(R.id.delete);
 
-            code =itemList.get(position).itemCode;
-            name = itemList.get(position).itemName;
-            quentity  = itemList.get(position).iteQuty;
-            rate = itemList.get(position).itemRate;
-            dist = itemList.get(position).itemDisc;
+                code = itemList.get(position).itemCode;
+                name = itemList.get(position).itemName;
+                quentity = itemList.get(position).iteQuty;
+                rate = itemList.get(position).itemRate;
+                dist = itemList.get(position).itemDisc;
 
-            purName =  "pro_name =" + name + ";";
-            purcode =  "pro_code =" + code +  ";";
-            purQuty =  "pro_qty ="  + quentity +";";
-            purrate =  "pro_rate =" + rate +";";
-            purdist = "pro_disc =" + dist ;
+                purName = "pro_name =" + name + ";";
+                purcode = "pro_code =" + code + ";";
+                purQuty = "pro_qty =" + quentity + ";";
+                purrate = "pro_rate =" + rate + ";";
+                purdist = "pro_disc =" + dist;
 
-            String all_details = "{"+purName + purcode + purQuty+ purrate+ purdist+"}";
-            testItem.add(all_details);
-            notifyDataSetChanged();
+                String all_details = "{" + purName + purcode + purQuty + purrate + purdist + "}";
+                testItem.add(all_details);
+                System.out.println("The if array is " +testItem);
+                notifyDataSetChanged();
 
-            SharedPreferences pref = getContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-            SharedPreferences.Editor editor = pref.edit();
-            editor.putString("ItemListFinalValue", String.valueOf(testItem));
-            editor.clear();
-            editor.commit();
-            rowBeforeDel = itemList.size();
-            tex_rowCount.setText("TotalCount :" +rowBeforeDel);
+                SharedPreferences pref = getContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("ItemListFinalValue", String.valueOf(testItem));
+                editor.clear();
+                editor.commit();
 
-            for(int i=0;i<rowBeforeDel;i++){
-                int val = Integer.parseInt(itemList.get(i).getIteQuty());
-                Quantity.add(val);
-                System.out.println("The total quantity of array: " + Quantity +"  hi");
+                rowBeforeDel = itemList.size();
 
+                for (int i = 0; i < rowBeforeDel; i++) {
+                    int val = Integer.parseInt(itemList.get(i).getIteQuty());
+                    Quantity.add(val);
+                    System.out.println("The total quantity of array: " + Quantity + "  hi");
+
+                }
+
+                for (int j = 0; j < Quantity.size(); j++) {
+                    int id = Quantity.get(j);
+                    test2 = test2 + id;
+                    System.out.println("The total quantity of array tetst: " + test2);
+                    txt_total.setText("Total Quantity:" +test2);
+                }
+                view.setTag(holder);
+            }
+            else
+            {
+                holder = (ViewHolder) view.getTag();
             }
 
-            for(int j = 0; j< Quantity.size(); j++){
-                int id = Quantity.get(j);
-                test2 = test2 + id;
-                System.out.println("The total quantity of array tetst: " + test2);
-                txt_total.setText(test2+"");
+            holder.itemcode.setText(itemList.get(position).getItemCode());
+            holder.itemname.setText(itemList.get(position).getItemName());
+            holder.itemqut.setText(itemList.get(position).getIteQuty());
+            holder.itemrate.setText(itemList.get(position).getItemRate());
+            holder.itemdis.setText(itemList.get(position).getItemDisc());
+
+            holder.img_delete.setTag(position);
+            holder.img_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                            mContext);
+
+                    alertDialogBuilder.setTitle("Delete");
+
+                    alertDialogBuilder
+                            .setCancelable(false)
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+
+                                    itemList.remove(position);
+                                    notifyDataSetChanged();
+                                    int deletecount = itemList.size();
+                                    tex_rowCount.setText("TotalCount: " +deletecount);
+
+                                }
+                            })
+                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+
+                    alertDialog.show();
+                }
+
+            });
+
+            return view;
+        }
+        else {
+            System.out.println("The adapter paramete else : " +parmeter);
+
+            if (view == null) {
+                view = LayoutInflater.from(mContext).inflate(R.layout.activity_item, viewGroup, false);
+                holder = new ViewHolder();
+
+                holder.itemname = (TextView) view.findViewById(R.id.txtnames);
+                holder.itemcode = (TextView) view.findViewById(R.id.txtCodes);
+                holder.itemqut = (TextView) view.findViewById(R.id.txtQuty);
+                holder.itemdis = (TextView) view.findViewById(R.id.txtDisc);
+                holder.itemrate = (TextView) view.findViewById(R.id.txtRate);
+                holder.img_delete = (ImageView)view.findViewById(R.id.delete);
+
+                holder.itemdis.setVisibility(View.GONE);
+                holder.itemrate.setVisibility(View.GONE);
+                code =itemList.get(position).itemCode;
+                name = itemList.get(position).itemName;
+                quentity  = itemList.get(position).iteQuty;
+                rate = itemList.get(position).itemRate;
+                dist = itemList.get(position).itemDisc;
+
+                purName =  "pro_name =" + name + ";";
+                purcode =  "pro_code =" + code +  ";";
+                purQuty =  "pro_qty ="  + quentity +";";
+                purrate =  "pro_rate =" + rate +";";
+                purdist = "pro_disc =" + dist ;
+                String all_details = "{"+purName + purcode + purQuty+ purrate+ purdist+"}";
+                testItems.add(all_details);
+                System.out.println("The else array is " +testItems);
+                notifyDataSetChanged();
+
+                rowBeforeDel = itemList.size();
+
+                for (int i = 0; i < rowBeforeDel; i++) {
+                    int val = Integer.parseInt(itemList.get(i).getIteQuty());
+                    Quantity.add(val);
+                    System.out.println("The total quantity of array: " + Quantity + "  hi");
+
+                }
+
+                for (int j = 0; j < Quantity.size(); j++) {
+                    int id = Quantity.get(j);
+                    test2 = test2 + id;
+                    System.out.println("The total quantity of array tetst: " + test2);
+                    txt_total.setText("Total Quantity: " +test2);
+                }
+
+
+                SharedPreferences pref = getContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("ItemListFinalValue", String.valueOf(testItems));
+                editor.clear();
+                editor.commit();
+
+
+                view.setTag(holder);
+            }
+            else
+            {
+                holder = (ViewHolder)view.getTag();
             }
 
+            holder.itemcode.setText(itemList.get(position).getItemCode());
+            holder.itemname.setText(itemList.get(position).getItemName());
+            holder.itemqut.setText(itemList.get(position).getIteQuty());
+            holder.itemrate.setText(itemList.get(position).getItemRate());
+            holder.itemdis.setText(itemList.get(position).getItemDisc());
 
+            holder.img_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                            mContext);
 
-            view.setTag(holder);
+                    alertDialogBuilder.setTitle("Delete");
 
+                    alertDialogBuilder
+                            .setCancelable(false)
+                            .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id)
+                                {
+                                    itemList.remove(position).getIteQuty();
+                                    notifyDataSetChanged();
+                                    int deletecount = itemList.size();
+                                    tex_rowCount.setText("TotalCount: " +deletecount);
+                                }
+                            })
+                            .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+
+                    alertDialog.show();
+                }
+
+            });
 
         }
-        else
-        {
-            holder = (ViewHolder)view.getTag();
-
-        }
-
-        holder.itemcode.setText(itemList.get(position).getItemCode());
-        holder.itemname.setText(itemList.get(position).getItemName());
-        holder.itemqut.setText(itemList.get(position).getIteQuty());
-        holder.itemrate.setText(itemList.get(position).getItemRate());
-        holder.itemdis.setText(itemList.get(position).getItemDisc());
-
-        holder.img_delete.setTag(position);
-        holder.img_delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        mContext);
-
-                alertDialogBuilder.setTitle("Delete");
-
-                alertDialogBuilder
-                        .setCancelable(false)
-                        .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
-
-                                int test = Integer.parseInt(itemList.get(position).getIteQuty());
-                                itemList.remove(position);
-                                notifyDataSetChanged();
-                                txt_total.setText("");
-
-                            }
-                        })
-                        .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
-
-                                dialog.cancel();
-                            }
-                        });
-
-                AlertDialog alertDialog = alertDialogBuilder.create();
-
-                alertDialog.show();
-            }
-
-        });
-
         return view;
     }
+
+
+
+
 
     class ViewHolder {
 
